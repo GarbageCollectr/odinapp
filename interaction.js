2e9d178620d80a05445f401e327e4d4360133dc5
Alex Werner <alexwerner569@hotmail.com>
2022-09-11 15:53:52 -0700

    added .js file

--- interaction.js
+++ interaction.js
@@ -71,6 +71,8 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', function(e){
